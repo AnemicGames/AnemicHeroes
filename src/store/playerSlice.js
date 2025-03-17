f81cfa68f2a7298eb60c51d9e81ea962c3796773
Thomas Eikhaugen <Thomas.Eikhaugen@gmail.com>
2025-03-17 12:58:33 +0100

    added player stats and equipped slots to playerSlice

--- src/store/playerSlice.js
+++ src/store/playerSlice.js
@@ -1,14 +1,21 @@
 export const createPlayerSlice = (set, get) => ({
-    player: {
-      name: 'Hero',
-      level: 1,
-      currentHp: 100,
-      maxHp: 100,
-      attack: 10,
-      defense: 5,
-      speed: 5,
-      xp: 0,
-      xpToNextLvl: 100,
+  player: {
+    name: 'Hero',
+    class: '',
+    level: 0,
+    currentHp: 100,
+    maxHp: 100,
+    strength: 10,
+    defense: 5,
+    speed: 5,
+    xp: 0,
+    xpToNextLvl: 100,
+    equipped: {
+      weapon: null,
+      helmet: null,
+      armor: null,
+      boots: null,
+      trinket: null,
     },
   });
   
