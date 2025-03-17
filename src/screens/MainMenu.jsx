c4f87d248a9d35613deb46cf9d530511075670fb
Thomas Eikhaugen <Thomas.Eikhaugen@gmail.com>
2025-03-17 15:18:50 +0100

    feat: added bg animation to main menu

--- src/screens/MainMenu.jsx
+++ src/screens/MainMenu.jsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useGameStore } from '../store/useGameStore';
+
+const AnimatedImage = () => {
+  const [currentFrame, setCurrentFrame] = useState(1);
+  const totalFrames = 10;
+  const frameRate = 170;
+  useEffect(() => {
+    const imageCache = [];
+    for (let i = 1; i <= totalFrames; i++) {
+      const img = new Image();
+      img.src = `/assets/bonfire_bg/${i}.webp`;
+      imageCache.push(img);
+    }
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentFrame((prevFrame) => (prevFrame % totalFrames) + 1);
+    }, frameRate);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <img
+      src={`/assets/bonfire_bg/${currentFrame}.webp`}
+      alt="Animated bonfire"
+      className="w-full"
+    />
+  );
+};
 
 export default function MainMenu() {
   const setCurrentView = useGameStore((state) => state.setCurrentView);
@@ -14,8 +44,9 @@ export default function MainMenu() {
 
   return (
     <div className="h-full w-full relative">
-      <img src="/assets/bonfire-bg.webp" alt="" className='w-full' />
-      <div className='absolute top-[130px] flex flex-col gap-2 left-[250px]' >
+      <AnimatedImage />
+
+      <div className='absolute top-[130px] flex flex-col gap-2 left-[250px]'>
         <button
           className="px-6 py-3 bg-green-700 hover:bg-green-800 rounded flex"
           onClick={goToMap}
