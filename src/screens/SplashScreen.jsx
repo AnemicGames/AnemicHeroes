8b60aa2b082258eb494cd3b6edfe79360c92a204
Thomas Eikhaugen <Thomas.Eikhaugen@gmail.com>
2025-03-17 13:26:51 +0100

    feat: added book sprite to continue and feather to new hero buttons. Added css to toggle book icons

--- src/screens/SplashScreen.jsx
+++ src/screens/SplashScreen.jsx
@@ -30,19 +30,26 @@ export default function SplashScreen() {
         <div className={`${styles["fade-in-container"]} flex flex-col items-center justify-center w-fit h-full p-4`}>
           <img src="assets/splashScreenTitle.png" alt="" className={`${styles["title-descend"]} w-[450px]`} />
           <div className={`${styles["fade-in-buttons"]} flex flex-col items-center space-y-2`}>
+            
             <button
-              className={`px-4 py-2 rounded ${
+              className={`px-4 py-2 rounded flex items-center ${
                 playerLevel > 0 ? 'bg-red-950 hover:bg-red-900' : 'bg-gray-700 cursor-not-allowed'
               }`}
               onClick={handleContinue}
               disabled={playerLevel === 0}
             >
-              Continue
+              <div className={styles["book-container"]}>
+                <img src="/assets/sprites/book-closed.png" alt="" className={styles["book-icon"]} />
+                <img src="/assets/sprites/book-open.png" alt="" className={styles["book-icon-hover"]} />
+              </div>
+              <span className="ml-2">Continue</span>
             </button>
+
             <button
-              className="px-4 py-2 bg-gray-700 hover:bg-gray-800 rounded"
+              className="px-4 py-2 bg-gray-700 hover:bg-gray-800 rounded flex items-center"
               onClick={handleCreateHero}
             >
+              <img src="/assets/sprites/feather.png" alt="" className="w-6 h-6 mr-2" />
               New Hero
             </button>
           </div>
