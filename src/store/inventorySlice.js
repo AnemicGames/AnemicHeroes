b78ad4c115df7284dd3d0cddceb5161162dd240b
Thomas Eikhaugen <Thomas.Eikhaugen@gmail.com>
2025-03-18 06:34:58 +0100

    feat: added inventory, addItem and removeItem to inventorySlice

--- src/store/inventorySlice.js
+++ src/store/inventorySlice.js
@@ -1,3 +1,44 @@
 export const createInventorySlice = (set, get) => ({
+  inventory: {
+    items: {}, // { "WPN_SWORD": 2, "ARM_PLATE": 1 }
+  },
 
-  });
+  addItem: (itemId, count = 1) => {
+    set((state) => {
+      const updatedItems = { ...state.inventory.items };
+
+      if (updatedItems[itemId]) {
+        updatedItems[itemId] += count;
+      } else {
+        updatedItems[itemId] = count;
+      }
+
+      return {
+        inventory: {
+          ...state.inventory,
+          items: updatedItems,
+        },
+      };
+    });
+  },
+
+  removeItem: (itemId, count = 1) => {
+    set((state) => {
+      const updatedItems = { ...state.inventory.items };
+
+      if (updatedItems[itemId]) {
+        updatedItems[itemId] -= count;
+        if (updatedItems[itemId] <= 0) {
+          delete updatedItems[itemId];
+        }
+      }
+
+      return {
+        inventory: {
+          ...state.inventory,
+          items: updatedItems,
+        },
+      };
+    });
+  },
+});
