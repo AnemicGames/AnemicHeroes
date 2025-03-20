5837fe916313afb68c13ae54c07ac14c0c4cea88
Thomas Eikhaugen <Thomas.Eikhaugen@gmail.com>
2025-03-20 18:44:07 +0100

    chore: added assetsManifest

---
+++ src/assetsManifest.js
@@ -0,0 +1,94 @@
+export const assetsManifest = {
+    images: [
+        // all your .png, .webp, etc. image paths
+        '/assets/battle_bg/1.webp',
+        '/assets/battle_bg/2.webp',
+        '/assets/battle_bg/3.webp',
+        '/assets/battle_bg/4.webp',
+        '/assets/battle_bg/5.webp',
+        '/assets/battle_bg/6.webp',
+        '/assets/battle_bg/7.webp',
+        '/assets/battle_bg/8.webp',
+        '/assets/battle_bg/9.webp',
+        '/assets/battle_bg/10.webp',
+        '/assets/battle_bg/11.webp',
+        '/assets/battle_bg/12.webp',
+        '/assets/battle_bg/13.webp',
+
+        // Bonfire BG
+        '/assets/bonfire_bg/1.webp',
+        '/assets/bonfire_bg/2.webp',
+        '/assets/bonfire_bg/3.webp',
+        '/assets/bonfire_bg/4.webp',
+        '/assets/bonfire_bg/5.webp',
+        '/assets/bonfire_bg/6.webp',
+        '/assets/bonfire_bg/7.webp',
+        '/assets/bonfire_bg/8.webp',
+        '/assets/bonfire_bg/9.webp',
+        '/assets/bonfire_bg/10.webp',
+
+        // Single background images in /assets
+        '/assets/bonfire-bg.webp',
+        '/assets/castle-bg-dither-2px.webp',
+        '/assets/splashScreenTitle.png',
+
+        // Armor
+        '/assets/sprites/armor/cloth_arm.png',
+        '/assets/sprites/armor/cloth_bts.png',
+        '/assets/sprites/armor/cloth_hlm.png',
+        '/assets/sprites/armor/iron_arm.png',
+        '/assets/sprites/armor/iron_bts.png',
+        '/assets/sprites/armor/iron_hlm.png',
+        '/assets/sprites/armor/leather_arm.png',
+        '/assets/sprites/armor/leather_bts.png',
+        '/assets/sprites/armor/leather_hlm.png',
+
+        // Bosses
+        '/assets/sprites/bosses/dark_lord.png',
+        '/assets/sprites/bosses/frost_wyrm.png',
+        '/assets/sprites/bosses/giant.png',
+        '/assets/sprites/bosses/vampire.png',
+
+        // Heroes
+        '/assets/sprites/heroes/mage.png',
+        '/assets/sprites/heroes/rogue.png',
+        '/assets/sprites/heroes/warrior.png',
+
+        // Mobs
+        '/assets/sprites/mobs/fallen_knight.png',
+        '/assets/sprites/mobs/forest_elemental.png',
+        '/assets/sprites/mobs/ghost.png',
+        '/assets/sprites/mobs/necromancer.png',
+        '/assets/sprites/mobs/shade.png',
+        '/assets/sprites/mobs/shadowy_figure.png',
+        '/assets/sprites/mobs/sorcerer.png',
+        '/assets/sprites/mobs/stone_golem.png',
+        '/assets/sprites/mobs/succubus.png',
+
+        // Potions
+        '/assets/sprites/potions/hp_pot.png',
+
+        // Trinkets
+        '/assets/sprites/trinkets/rouge_trk.png',
+        '/assets/sprites/trinkets/warr_trk.png',
+        '/assets/sprites/trinkets/wiz_trk.png',
+
+        // Misc sprite files
+        '/assets/sprites/book-closed.png',
+        '/assets/sprites/book-open.png',
+        '/assets/sprites/embark-nav-icon.png',
+        '/assets/sprites/exit-nav-icon.png',
+        '/assets/sprites/feather.png',
+        '/assets/sprites/inventory-nav-icon.png',
+        '/assets/sprites/shop-nav-icon.png',
+        '/assets/sprites/sword-shiny.png',
+
+    ],
+    data: [
+        '/assets/bosses.json',
+        '/assets/items.json',
+        '/assets/mobs.json',
+    ],
+    audio: [
+    ],
+};
