34383804b2350208b3cbe2adc9f8ae8e09a9d481
JNettli <jonas.nettli@gmail.com>
2025-03-25 11:07:34 +0100

    feat: added styling to map sprites and "you are here" styling

--- src/screens/MapView.jsx
+++ src/screens/MapView.jsx
@@ -56,7 +56,6 @@ export default function MapView() {
         setIsVisible(true);
     }, []);
 
-
     const navigateWithAnimation = (view) => {
         setIsClosing(true);
         setTimeout(() => {
@@ -70,7 +69,7 @@ export default function MapView() {
     useEffect(() => {
         const fetchMap = async () => {
             if (!Array.isArray(map) || map.length === 0) {
-            await initializeMap();
+                await initializeMap();
             }
             setIsLoading(false);
         };
@@ -82,7 +81,10 @@ export default function MapView() {
         if (cell.type != 0 && currentCell.next.includes(cell.id)) {
             setPosition(cell.id);
         }
-        if (cell.type === "BATTLE" && currentCell.next.includes(cell.id)|| cell.type === "BOSS" && currentCell.next.includes(cell.id)) {
+        if (
+            (cell.type === "BATTLE" && currentCell.next.includes(cell.id)) ||
+            (cell.type === "BOSS" && currentCell.next.includes(cell.id))
+        ) {
             goToBattle();
             setEncounterType(cell.encounterType);
             setEncounterDifficulty(cell.encounterDifficulty);
@@ -101,12 +103,11 @@ export default function MapView() {
         return currentCell && currentCell.next.includes(cell.id);
     };
 
-    const startPosition = map.flat().find(cell => cell.type === "START");
+    const startPosition = map.flat().find((cell) => cell.type === "START");
 
     const goToMainMenu = () => navigateWithAnimation("MAIN_MENU");
     const goToBattle = () => navigateWithAnimation("BATTLE");
     const goToShop = () => navigateWithAnimation("SHOP");
-
 
     return (
         <div className="relative h-full w-full">
@@ -114,9 +115,13 @@ export default function MapView() {
 
             <div
                 className={`absolute w-full h-max ${
-                    isClosing ? styles["mapClose"] : isVisible ? styles["mapOpen"] : ""
+                    isClosing
+                        ? styles["mapClose"]
+                        : isVisible
+                        ? styles["mapOpen"]
+                        : ""
                 }`}
->
+            >
                 <img
                     src="/assets/map.webp"
                     alt="map background"
@@ -133,29 +138,61 @@ export default function MapView() {
                                 <div
                                     key={cell.id}
                                     onClick={() => handleTileClick(cell)}
-                                    className={`relative w-10 h-10 flex items-center justify-center rounded-xl ${
-                                        isPlayer
-                                            ? "border-2 bg-blue-800/80 cursor-pointer p-[.75px]"
-                                            : isWalkable
-                                            ? "border-2 bg-gray-100/80 cursor-pointer p-[.75px]"
-                                            : cell.type === "START"
-                                            ? "border-2 bg-green-950/80 cursor-pointer p-[.75px]"
-                                            : cell.type === "BATTLE"
-                                            ? "border-2 bg-gray-900/80 cursor-pointer p-[.75px]"
-                                            : cell.type === "BOSS"
-                                            ? "border-2 bg-red-950 cursor-pointer"
-                                            : cell.type === "SHOP"
-                                            ? "border-2 bg-gray-100/80 cursor-pointer p-[.75px]"
-                                            : cell.type === "EVENT"
-                                            ? "border-2 bg-amber-400-80 cursor-pointer p-[.75px]"
-                                            : "transparent"
-                                    }`}
+                                    className="relative w-10 h-10 flex items-center justify-center rounded-xl"
                                 >
-                                    {cell.type === "START" ? <img src={'./assets/sprites/map/fc911.png'} alt="START" /> : ""}
-                                    {cell.type === "BATTLE" ? <img src={'./assets/sprites/map/fc729.png'} alt="BATTLE" /> : ""}
-                                    {cell.type === "EVENT" ? <img src={'./assets/sprites/map/fc13.png'} alt="RANDOM" /> : ""}
-                                    {cell.type === "SHOP" ? <img src={'./assets/sprites/map/fc133.png'} alt="SHOP" /> : ""}
-                                    {cell.type === "BOSS" ? <img src={'./assets/sprites/map/fc1231.png'} alt="Boss" /> : ""}
+                                    {isPlayer && (
+                                        <div className="absolute -inset-0 outline-red-600 z-1 outline-6 animate-pulse rounded-xl"></div>
+                                    )}
+                                    <div
+                                        className={`relative w-full h-full flex items-center justify-center rounded-xl ${
+                                            isPlayer
+                                                ? "border-2 border-amber-950/90 bg-blue-800/80 cursor-pointer p-[.75px]"
+                                                : isWalkable
+                                                ? "border-2 border-amber-950/90 bg-amber-200/80 cursor-pointer p-[.75px]"
+                                                : cell.type === "START"
+                                                ? "border-2 border-amber-950/90 bg-green-950/80 cursor-pointer p-[.75px]"
+                                                : cell.type === "BATTLE"
+                                                ? "border-2 border-amber-950/90 bg-amber-800/70 cursor-not-allowed p-[.75px]"
+                                                : cell.type === "BOSS"
+                                                ? "border-2 border-amber-950/90 bg-red-600 cursor-pointer"
+                                                : cell.type === "SHOP"
+                                                ? "border-2 border-amber-950/90 bg-yellow-400/80 cursor-pointer p-[.75px]"
+                                                : cell.type === "EVENT"
+                                                ? "border-2 border-amber-950/90 bg-amber-400/80 cursor-pointer p-[.75px]"
+                                                : "transparent"
+                                        }`}
+                                    >
+                                        {cell.type === "START" && (
+                                            <img
+                                                src="./assets/sprites/map/fc911.png"
+                                                alt="START"
+                                            />
+                                        )}
+                                        {cell.type === "BATTLE" && (
+                                            <img
+                                                src="./assets/sprites/map/fc729.png"
+                                                alt="BATTLE"
+                                            />
+                                        )}
+                                        {cell.type === "EVENT" && (
+                                            <img
+                                                src="./assets/sprites/map/fc13.png"
+                                                alt="RANDOM"
+                                            />
+                                        )}
+                                        {cell.type === "SHOP" && (
+                                            <img
+                                                src="./assets/sprites/map/fc133.png"
+                                                alt="SHOP"
+                                            />
+                                        )}
+                                        {cell.type === "BOSS" && (
+                                            <img
+                                                src="./assets/sprites/map/fc1231.png"
+                                                alt="Boss"
+                                            />
+                                        )}
+                                    </div>
                                 </div>
                             );
                         })
@@ -165,11 +202,15 @@ export default function MapView() {
 
             {startPosition && currentPosition.id === startPosition.id && (
                 <button
-                    className='absolute bottom-0 left-0 m-4 p-2 hover:bg-red-800 rounded z-50'
-                    title='Exit'
-                    onClick={() => goToMainMenu('MAIN_MENU')}
+                    className="absolute bottom-0 left-0 m-4 p-2 hover:bg-red-800 rounded z-50"
+                    title="Exit"
+                    onClick={() => goToMainMenu("MAIN_MENU")}
                 >
-                    <img src="/assets/sprites/exit-nav-icon.png" alt="Exit" className='w-10 h-11' />
+                    <img
+                        src="/assets/sprites/exit-nav-icon.png"
+                        alt="Exit"
+                        className="w-10 h-11"
+                    />
                 </button>
             )}
         </div>
