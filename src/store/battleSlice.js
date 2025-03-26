abd414adb01df7ff1fba0e74f4f2d6e952953f9e
LAPTOP-5RU3UB56\Berke <berken_93@hotmail.com>
2025-03-26 16:33:09 +0100

    Fixed the gold calculations. On victory now gives the player the correct amount of gold.

--- src/store/battleSlice.js
+++ src/store/battleSlice.js
@@ -1,25 +1,24 @@
 export const createBattleSlice = (set, get) => ({
   enemy: {
-    id: "m1",
-    name: "Fallen knight",
-    baseHP: 150,
-    baseStrength: 12,
-    baseSpeed: 10,
-    baseDefence: 12,
-    lvlMultiplier: 1.1,
-    dropChance: 1.1,
-    baseGold: 10,
+    id: "id",
+    name: "placeholder",
+    baseHP: 1,
+    baseStrength: 1,
+    baseSpeed: 1,
+    baseDefence: 1,
+    lvlMultiplier: 1,
+    dropChance: 1,
+    baseGold: 1,
     sprite: "",
 
-    currentHP: 0,
+    currentHP: 1,
   },
   gameOver: false,
   turnCount: 0,
   nextToAttack: null,
   isFighting: true,
   skipTurn: false,
-  xp: 50,
-  baseGold: 50,
+  xp: 1,
 
   setXP: (xp) => {
     set((state) => {
@@ -84,19 +83,42 @@ export const createBattleSlice = (set, get) => ({
     }),
 
   setEnemy: (enemy) => set({ enemy, currentHP: enemy.baseHP }),
-  
+
+  clearBattle: () =>
+    set(() => ({
+      enemy: {
+        id: "id",
+        name: "placeholder",
+        baseHP: 1,
+        baseStrength: 1,
+        baseSpeed: 1,
+        baseDefence: 1,
+        lvlMultiplier: 1,
+        dropChance: 1,
+        baseGold: 1,
+        sprite: "",
+        currentHP: 1,
+      },
+      gameOver: false,
+      turnCount: 0,
+      nextToAttack: null,
+      isFighting: false,
+      skipTurn: false,
+      xp: 1,
+    })),
+    
 
   damageEnemy: (amount) =>
     set((state) => {
-      const newHealth = Math.max(0, state.enemy.currentHP - amount);
-      const enemyDefeated = newHealth === 0;
-      if (enemyDefeated) {
-        console.log(`${state.enemy.name} has been defeated!`);
-      }
-      return {
-        enemy: { ...state.enemy, currentHP: newHealth },
-        gameOver: enemyDefeated,
-      };
+        const newHealth = Math.max(0, state.enemy.currentHP - amount);
+        const enemyDefeated = newHealth === 0;
+        if (enemyDefeated) {
+            console.log(`${state.enemy.name} has been defeated!`);
+        }
+        return {
+            enemy: { ...state.enemy, currentHP: newHealth },
+            gameOver: enemyDefeated,
+        };
     }),
 
   endBattle: () =>
